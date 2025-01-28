24264833d32024337fd0e2afcf2944ceea225d1c
Enes Arslan <arslanenes515@gmail.com>
2025-01-28 15:17:36 +0300

    feat: browser-use example with your own browser

--- browser-agent.py
+++ browser-agent.py
@@ -1,4 +1,4 @@
-from browser_use import Agent
+from browser_use import Agent, Browser, BrowserConfig
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
 from pydantic import SecretStr
@@ -7,6 +7,15 @@ import os
 import asyncio
 
 load_dotenv()  # load environment variables from .env file
+
+browser = Browser(
+    config=BrowserConfig(
+        # Specify the path to your Chrome executable
+        chrome_instance_path="/usr/bin/google-chrome"
+        # For MacOS, '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        # For Windows, typically: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    )
+)
 
 llm = AzureChatOpenAI(
     model=os.getenv("AZURE_OPENAI_DEPLOYMENT", ""),
@@ -18,8 +27,9 @@ llm = AzureChatOpenAI(
 
 async def main():
     agent = Agent(
-        task="Compare pricing informations for followed products: jira, linear, clickup",
+        task="Create a Google Form that aims get name and email of the user. Name it 'Let's meet with Actins!' and publish. Return a link to the published form",
         llm=llm,
+        browser=browser,
         save_conversation_path="logs/conversations.json",
     )
     result = await agent.run()
